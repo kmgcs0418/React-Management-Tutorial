410a1a2933c2375eeabf1619bff59bdf44495b9b
kmgcs0418 <kmgcs8626@naver.com>
2022-01-16 17:49:20 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop managment system!</h2>
-    </div>
-  );
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '김민규',
+  'birthday': '970418',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '970419',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '가나다',
+  'birthday': '970420',
+  'gender': '남자',
+  'job': '디자이너'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+      
+    );
+  }
 }
 
 export default App;
